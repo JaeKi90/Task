94eca48a67e6d060aaf757c0498420d29d60e8c9
JaeKi <jaeki.lee90@gmail.com>
2022-09-20 07:22:28 +0900

    recruitType cell work

--- Task/View/Cell/CompanyTypeTableViewCell.swift
+++ Task/View/Cell/CompanyTypeTableViewCell.swift
@@ -16,55 +16,78 @@ class CompanyTypeTableViewCell: UITableViewCell {
     
     @IBOutlet weak var updateDateLabel: UILabel!
     @IBOutlet weak var reviewSummaryLabel: UILabel!
-    @IBOutlet weak var salaryLabel: UILabel!
     
+    @IBOutlet weak var companyTypeView: UIView!
+    @IBOutlet weak var salaryLabel: UILabel!
     @IBOutlet weak var interviewQuestionLabel: UILabel!
+    
+    @IBOutlet weak var reviewTypeView: UIView!
+    @IBOutlet weak var prosPointLabel: UILabel!
+    @IBOutlet weak var consPointLabel: UILabel!
     
     static let defaultReuseIdentifier = "CompanyTypeTableViewCell"
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        self.selectionStyle = .none
     }
     
     func render(cellItem: CellItem) {
-        if let logoImagePath = cellItem.logoPath,
+        
+        guard let logoImagePath = cellItem.logoPath,
               let rate = cellItem.rateTotalAvg,
               let companyName = cellItem.name,
               let industryName = cellItem.industryName,
               let updateDate = cellItem.updateDate,
-              let reviewSummary = cellItem.reviewSummary,
-              let salaryAvg = cellItem.salaryAvg,
-              let interviewQuestion = cellItem.interviewQuestion
-        {
-            //image
-            self.logoImageView.layer.cornerRadius = 4
-            self.logoImageView.layer.borderColor = UIColor.Gray03.cgColor
-            self.logoImageView.layer.borderWidth = 1
-
-            self.logoImageView.tintColor = .lightGray.withAlphaComponent(0.5)
-            self.logoImageView.setImage(
-                with: logoImagePath,
-                placeholder: UIImage(systemName: "circle.dashed")) { [weak self] result in
-                    guard let image = try? result.get().image else { return }
-                    self?.logoImageView.image = image
+              let reviewSummary = cellItem.reviewSummary else { return }
+        
+        //image
+        self.logoImageView.layer.cornerRadius = 4
+        self.logoImageView.layer.borderColor = UIColor.Gray03.cgColor
+        self.logoImageView.layer.borderWidth = 1
+        
+        self.logoImageView.tintColor = .lightGray.withAlphaComponent(0.5)
+        self.logoImageView.setImage(
+            with: logoImagePath,
+            placeholder: UIImage(systemName: "circle.dashed")) { [weak self] result in
+                guard let image = try? result.get().image else { return }
+                self?.logoImageView.image = image
             }
-
-            self.companyNameLabel.text = companyName
-
-            self.industryLabel.text = industryName
-
-            let updatedDate = updateDate.components(separatedBy: "T")
-            var convertedUpdateDate = updatedDate[0]
-            convertedUpdateDate = convertedUpdateDate.replacingOccurrences(of: "-", with: ".")
-            self.updateDateLabel.text = convertedUpdateDate
-
-            self.reviewSummaryLabel.text = reviewSummary
-
-            self.salaryLabel.text = "\(salaryAvg)"
+        
+        self.companyNameLabel.text = companyName
+        
+        self.industryLabel.text = industryName
+        
+        let updatedDate = updateDate.components(separatedBy: "T")
+        var convertedUpdateDate = updatedDate[0]
+        convertedUpdateDate = convertedUpdateDate.replacingOccurrences(of: "-", with: ".")
+        self.updateDateLabel.text = convertedUpdateDate
+        
+        self.reviewSummaryLabel.text = reviewSummary
+        
+        if cellItem.cellType == .cellTypeCompany {
+            self.reviewTypeView.isHidden = true
+            self.companyTypeView.isHidden = false
             
-            self.interviewQuestionLabel.text = interviewQuestion
+            if let salaryAvg = cellItem.salaryAvg,
+               let interviewQuestion = cellItem.interviewQuestion
+            {
+                self.salaryLabel.text = "\(salaryAvg)"
+                self.interviewQuestionLabel.text = interviewQuestion
+            }
+        } else {
+            self.companyTypeView.isHidden = true
+            self.reviewTypeView.isHidden = false
+            
+            if let prosPoint = cellItem.pros,
+               let consPoint = cellItem.cons
+            {
+                self.prosPointLabel.text = prosPoint
+                self.consPointLabel.text = consPoint
+            }
         }
-    }
 
+    }
     
 }
